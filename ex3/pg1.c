f1fb5dedfba79daa4f92bb7648749eb3fcaf6e58
Santhosh Krishnan M <139557720+Santhosh-KrishnanM@users.noreply.github.com>
2026-04-16 12:09:01 +0530

    ex3

--- ex3/pg1.c
+++ ex3/pg1.c
@@ -1,79 +1,57 @@
-#include <mpi.h>
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-
+#include<stdio.h>
+#include<mpi.h>
+#include<stdlib.h>
+#include<time.h>
 #define MAX 100
 
-/*void UpperCase(char str[]) {
-    for (int i = 0; str[i] != '\0'; i++) {
-        str[i] = toupper(str[i]);
-    }
-}*/
-
-void UpperCase(char str[]){
-	for(int i=0; str[i]; i++){
-		if(str[i] >= 'a' && str[i] <= 'z')
-			str[i] = str[i] - ('a' - 'A');
-	}
+int main(int argc, char *argv[]){
+   int comm_sz, my_rank = 0, num, sfreq = 0, tfreq, i, n,N;
+   int arr[100];
+   int *parr;
+   MPI_Init(&argc,&argv);
+   MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+   //n = 12 / comm_sz;
+   //parr = (int*)malloc(n * sizeof(int));   
+   if(my_rank == 0){
+      srand(time(0));
+      printf("Enter the no. of elements: ");
+      fflush(stdout);
+      scanf("%d", &N);
+      if(N > 20){
+         for(i = 0; i < N; i++)
+            arr[i] = rand() % N;         
+      }else{
+	 for(i = 0; i < N; i++){
+            arr[i] = rand() % N;
+	    printf("%d ", arr[i]);
+         }
+   	 printf("\n");
+      }
+      printf("Enter the value to find: ");
+      fflush(stdout);
+      scanf("%d", &num);
+      if(N % comm_sz != 0){
+	      printf("\nThe given elements are not equally allocated to the processes\n");
+	      fflush(stdout);
+	      MPI_Abort(MPI_COMM_WORLD,1);
+      }
+      n = N / comm_sz;
+   } 
+   MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+   //n = N / comm_sz;
+   parr = (int*)malloc(n * sizeof(int));    
+   MPI_Scatter(arr, n, MPI_INT, parr, n, MPI_INT, 0, MPI_COMM_WORLD);
+   MPI_Bcast(&num, 1, MPI_INT, 0, MPI_COMM_WORLD);
+   for (i = 0; i < n; i++){
+      if(parr[i] == num)
+         sfreq++;
+   }
+   MPI_Reduce(&sfreq, &tfreq, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+   if(my_rank == 0)
+      printf("The frequency of the number %d in the list is %d\n", num, tfreq);
+   MPI_Finalize();
+   free(parr);
+   return 0;
 }
 
-int isPalindrome(char str[]) {
-    int left = 0;
-    int right = strlen(str) - 1;
-
-    while (left < right) {
-        if (str[left] != str[right])
-            return 0;
-        left++;
-        right--;
-    }
-    return 1;
-}
-
-int main() {
-    int rank, size;
-    char message[MAX];
-    MPI_Status status;
-
-    char pmessage[7][20] = {"Hi","hello","welcome","madam","malayalam","thank","program"};
-
-    MPI_Init(NULL, NULL);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-    if (rank != 0) {
-        if (rank % 2 == 1) {                
-            MPI_Send(pmessage[rank-1], strlen(pmessage[rank-1]) + 1, MPI_CHAR, 0, 1, MPI_COMM_WORLD);
-        } else {
-            //sprintf(message, "madam");
-	    //message[rank-1]
-            MPI_Send(pmessage[rank-1], strlen(pmessage[rank-1]) + 1, MPI_CHAR, 0, 2, MPI_COMM_WORLD);
-        }
-    }
-    
-    else {
-        for (int i = 1; i < size; i++) {
-            MPI_Recv(message, MAX, MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-
-            int sender = status.MPI_SOURCE;
-            int tag = status.MPI_TAG;
-	    char recv[MAX];
-
-            if (tag == 1) {
-                UpperCase(message);
-		printf("\n Uppercase conversion\nProcess %d(Type1):\n%s\n", sender,message);
-                //printf("From process %d (Type 1): Uppercase string = %s\n", sender, message);
-            } else if (tag == 2) {
-		printf("\n Palindrome checking\n");
-                if (isPalindrome(message))
-                    printf("Process %d (Type 2)\n%s is a Palindrome\n", sender, message);
-                else
-                    printf("Process %d (Type 2)\n%s is not a Palindrome\n", sender, message);
-            }
-        }
-    }
-
-    MPI_Finalize();
-    return 0;
-}
